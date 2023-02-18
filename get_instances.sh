cb8590579f6eaf6b928b4ddbe1a3ac2c1e2e034e
Isaac Joseph <Isaac.Joseph@affirm.com>
2023-02-18 11:57:20 -0800

    Better formatting

--- get_instances.sh
+++ get_instances.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -eu -o pipefail
 AUTH_HEADER=$(cat ./INSTANCES_SOCIAL_AUTH_HEADER.txt)
-curl -H "Authorization: Bearer ${AUTH_HEADER}" 'https://instances.social/api/1.0/instances/list?count=0&include_closed=true&include_down=false&sort_by=users&sort_order=desc' > all_instances.json
+curl -H "Authorization: Bearer ${AUTH_HEADER}" \
+ 'https://instances.social/api/1.0/instances/list?count=0&include_closed=true&include_down=false&sort_by=users&sort_order=desc' \
+ > all_instances.json
 
 # top 100
 jq '.instances[:100] | map({name})' all_instances.json |
