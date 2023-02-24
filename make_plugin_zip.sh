b68f66b4652473eedf1d149ac2d2a7b67d2558af
Isaac Joseph <Isaac.Joseph@affirm.com>
2023-02-23 16:34:35 -0800

    fix make plugin zip

--- make_plugin_zip.sh
+++ make_plugin_zip.sh
@@ -1,1 +1,1 @@
-git archive HEAD . ':!get_instances.sh' -o mastodon_social_icons.zip  # Wordpress doesn't allow bash scripts
+git archive HEAD . ':!get_instances.sh' ':!make_plugin_zip.sh' -o mastodon_social_icons.zip  # Wordpress doesn't allow bash scripts
